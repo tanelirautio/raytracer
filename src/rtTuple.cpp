e973435b9eac19d0de82d659a99cf4d13e696264
Taneli Rautio <taneli.rautio@gmail.com>
2021-08-02 01:54:54 +0300

    More functionality to Tuple class

--- src/rtTuple.cpp
+++ src/rtTuple.cpp
@@ -1,4 +1,5 @@
 #include "rtTuple.hpp"
+#include "rtTuple.hpp"
 
 #include <cmath>
 
@@ -13,13 +14,42 @@ namespace rt {
 		return false;
 	}
 
-	Tuple::Tuple(f32 x, f32 y, f32 z, f32 w) {
-		this->x = x;
-		this->y = y;
-		this->z = z;
-		this->w = w;
+	bool operator==(const Tuple lhs, const Tuple rhs)
+	{
+		return equal(lhs.x(), rhs.x()) &&
+			equal(lhs.y(), rhs.y()) &&
+			equal(lhs.z(), rhs.z()) &&
+			equal(lhs.w(), rhs.w());
 	}
 
+	Tuple operator+(const Tuple lhs, const Tuple rhs) {
+		return Tuple(lhs.x() + rhs.x(), lhs.y() + rhs.y(), lhs.z() + rhs.z(), lhs.w() + rhs.w());
+	}
+
+	Tuple operator-(const Tuple lhs, const Tuple rhs) {
+		return Tuple(lhs.x() - rhs.x(), lhs.y() - rhs.y(), lhs.z() - rhs.z(), lhs.w() - rhs.w());
+	}
+
+	Tuple operator-(const Tuple t) {
+		return Tuple(-t.x(), -t.y(), -t.z(), -t.w());
+	}
+
+	Tuple operator*(const Tuple t, f32 s) {
+		return Tuple(t.x() * s, t.y() * s, t.z() * s, t.w() * s);
+	}
+
+	Tuple operator/(const Tuple t, f32 s) {
+		return Tuple(t.x() / s, t.y() / s, t.z() / s, t.w() / s);
+	}
+
+	Tuple::Tuple(f32 x, f32 y, f32 z, f32 w) {
+		m_x = x;
+		m_y = y;
+		m_z = z;
+		m_w = w;
+	}
+
+	/*
 	Tuple Tuple::operator+(Tuple t) const {
 		return Tuple(x + t.x, y + t.y, z + t.z, w + t.w);
 	}	
@@ -39,28 +69,29 @@ namespace rt {
 	Tuple Tuple::operator/(f32 s) const {
 		return Tuple(x / s, y / s, z / s, w / s);
 	}
+	*/
 
 	f32 Tuple::magnitude() const {
-		return std::sqrt(x * x + y * y + z * z + w * w);
+		return std::sqrt(m_x * m_x + m_y * m_y + m_z * m_z + m_w * m_w);
 	}
 
 	Tuple Tuple::normalize() const {
 		f32 m = magnitude();
-		return Tuple(x / m, y / m, z / m, w / m);
+		return Tuple(m_x / m, m_y / m, m_z / m, m_w / m);
 	}
 
 	Vector Vector::normalize() const {
 		f32 m = magnitude();
-		return Vector(x / m, y / m, z / m);
+		return Vector(x() / m, y() / m, z() / m);
 	}
 
 	f32 Vector::dot(Vector b) const {
-		return (x * b.x + y * b.y + z * b.z);
+		return (x() * b.x() + y() * b.y() + z() * b.z());
 	}
 
 	Vector Vector::cross(Vector b) const {
-		return Vector(y * b.z - z * b.y, 
-			          z * b.x - x * b.z,
-					  x * b.y - y * b.x);
+		return Vector(y() * b.z() - z() * b.y(), 
+			          z() * b.x() - x() * b.z(),
+					  x() * b.y() - y() * b.x());
 	}
 }
--- src/rtTuple.hpp
+++ src/rtTuple.hpp
@@ -10,38 +10,61 @@ namespace rt {
 
 	class Tuple {
 		public:
-			Tuple() : x(0), y(0), z(0), w(0) {}
+			Tuple() : m_x(0), m_y(0), m_z(0), m_w(0) {}
 			Tuple(f32 x, f32 y, f32 z, f32 w);
 
+			/*
 			Tuple operator+(Tuple t) const;
 			Tuple operator-(Tuple t) const;
 			Tuple operator-() const;
 			Tuple operator*(f32 s) const;
 			Tuple operator/(f32 s) const;
+			*/
 
 			f32 magnitude() const;
 			Tuple normalize() const;
 
-			bool isPoint() { return w == 1; }
-			bool isVector() { return w == 0; }
+			bool isPoint() const { return m_w == 1; }
+			bool isVector() const { return m_w == 0; }
 
-			f32 x;
-			f32 y;
-			f32 z;
-			f32 w;
+			f32 x() const { return m_x; }
+			f32 y() const { return m_y; }
+			f32 z() const { return m_z; }
+			f32 w() const { return m_w; }
+
+		private:
+			f32 m_x, m_y, m_z, m_w;
 	};
+
+	bool operator==(const Tuple lhs, const Tuple rhs);
+	Tuple operator+(const Tuple lhs, const Tuple rhs);
+	Tuple operator-(const Tuple lhs, const Tuple rhs);
+	Tuple operator-(const Tuple t);
+	Tuple operator*(const Tuple t, f32 s);
+	Tuple operator/(const Tuple t, f32 s) ;
 
 	class Point : public Tuple {
 		public:
-			Point(f32 x, f32 y, f32 z) : Tuple(x, y, z, 1.0f) {}
+			Point() : Tuple(0, 0, 0, 1) {}
+			Point(f32 x, f32 y, f32 z) : Tuple(x, y, z, 1) {}
 	};
 
 	class Vector : public Tuple {
 		public:
-			Vector(f32 x, f32 y, f32 z) : Tuple(x, y, z, 0.0f) {}
+			Vector() : Tuple(0, 0, 0, 0) {}
+			Vector(f32 x, f32 y, f32 z) : Tuple(x, y, z, 0) {}
 			Vector normalize() const;
 			f32 dot(Vector b) const;
 			Vector cross(Vector b) const;
+	};
+
+	class Color : public Tuple {
+		public:
+			Color() : Tuple(0, 0, 0, 0) {}
+			Color(f32 x, f32 y, f32 z) : Tuple(x, y, z, 0) {}
+			f32 r() { return x(); }
+			f32 g() { return y(); }
+			f32 b() { return z(); }
 	};
 }
 
